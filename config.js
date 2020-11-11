9d24330fa05e78c5a9a9ac9f7af2e193da971a02
bhargavsai123 <61113181+bhargavsai123@users.noreply.github.com>
2020-11-11 14:14:14 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,21 @@
+/** @format */
+
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+
+var firebaseConfig = {
+	apiKey: 'AIzaSyBylijBM2CMC8Li4UM0Az32LXKyvcHte6k',
+	authDomain: 'storyhub-bd3de.firebaseapp.com',
+	databaseURL: 'https://storyhub-bd3de.firebaseio.com',
+	projectId: 'storyhub-bd3de',
+	storageBucket: 'storyhub-bd3de.appspot.com',
+	messagingSenderId: '272606914442',
+	appId: '1:272606914442:web:947e25f5618191d8824a73',
+};
+
+if (!firebase.apps.length) {
+	// Initialize Firebase
+	firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase.firestore();
